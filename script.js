2c20d5934443cd46e0c476a32148ea11e64b9ff3
Mansoor Ali Khan Pathan <mansoor786p.edu@gmail.com>
2025-09-17 14:11:05 +0530

    Update script.js
    update API url

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ async function predictImage() {
 
     document.getElementById('result').innerText = 'Predicting...';
     try {
-        const response = await fetch('http://localhost:8000/predict', {
+        const response = await fetch('https://drone-vs-bird-backend.onrender.com/predict', {
             method: 'POST',
             body: formData
         });
